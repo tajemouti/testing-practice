ed14f69eb16c7c6f19ffa3d3064fa743daeda6e9
tajemouti <tajemouti@gmail.com>
2023-07-27 03:08:50 +0100

    Add new test for stringLength function

--- stringLength.test.js
+++ stringLength.test.js
@@ -4,3 +4,11 @@ test(`Returns the correct string length`, () => {
   const string = 'microvere';
   expect(stringLength(string)).toBe(string.length);
 });
+
+test(`Generates an error when the string length is not within 0 and 10`, () => {
+    const string1 = 'microverseCirruculum';
+    const length = () => {
+      stringLength(string1);
+    };
+    expect(length).toThrowError();
+});
