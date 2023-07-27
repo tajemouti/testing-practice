d9c73ac9c2cf107344bc1ac4bd101b136e4db536
tajemouti <tajemouti@gmail.com>
2023-07-27 03:06:15 +0100

    Expand stringLength function

--- stringLength.js
+++ stringLength.js
@@ -1,4 +1,7 @@
 const stringLength = (string) => {
+    if (string.length < 1 || string.length > 10) {
+        throw new Error('String length must not exceed 10 characters');
+    };
     return string.length;
 };
   
